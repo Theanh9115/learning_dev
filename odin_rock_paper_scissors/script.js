6b35ef6c42613565b8313bd7b38ea6bbdc0f6e7d
The Anh Nguyen <anhngy9115@gmail.com>
2026-08-15 21:46:55 -0400

    UI for rock paper scissors

--- odin_rock_paper_scissors/script.js
+++ odin_rock_paper_scissors/script.js
@@ -1,3 +1,4 @@
+// Helpers
 function getComputerChoice(){
     let choice = Math.random();
     if (choice < 0.33){
@@ -11,55 +12,182 @@ function getComputerChoice(){
     }
 }
 
-// console.log(getComputerChoice());
-
-function getHumanChoice(){
-    let HumanChoice = prompt("Rock, Paper, or Scissors?");
-    return HumanChoice;
-}
-
-// console.log(getHumanChoice());
-
-let HumanScore = 0;
-let ComputerScore = 0;
-
-function playRound(HumanChoice, ComputerChoice){
+function playRound(HumanChoice, ComputerChoice, HumanScore, ComputerScore){
     let Insensitive_HumanChoice = HumanChoice.toUpperCase();
     let Insensitive_ComputerChoice = ComputerChoice.toUpperCase();
 
     if (Insensitive_HumanChoice == "ROCK" && Insensitive_ComputerChoice == "PAPER"){
         ComputerScore ++;
-        return "You Lose!!! Paper beats Rock";
+        return ["You Lose!!! Paper beats Rock", HumanScore, ComputerScore];
     }
     else if (Insensitive_HumanChoice == "ROCK" && Insensitive_ComputerChoice == "SCISSORS"){
         HumanScore ++;
-        return "You Win!!! Rock beats Scissors";
+        return ["You Win!!! Rock beats Scissors", HumanScore, ComputerScore];
     }
     else if (Insensitive_HumanChoice == "PAPER" && Insensitive_ComputerChoice == "ROCK"){
         HumanScore ++;
-        return "You Win!!! Paper beats Rock";
+        return ["You Win!!! Paper beats Rock", HumanScore, ComputerScore];
     }
     else if (Insensitive_HumanChoice == "PAPER" && Insensitive_ComputerChoice == "SCISSORS"){
         ComputerScore ++;
-        return "You Lose!!! Scissors beats Paper";
+        return ["You Lose!!! Scissors beats Paper",  HumanScore, ComputerScore];
     }
     else if (Insensitive_HumanChoice == "SCISSORS" && Insensitive_ComputerChoice == "ROCK"){
         ComputerScore ++;
-        return "You Lose!!! Rock beats Scissors";
+        return ["You Lose!!! Rock beats Scissors", HumanScore, ComputerScore];
     }
     else if (Insensitive_HumanChoice == "SCISSORS" && Insensitive_ComputerChoice == "PAPER"){
         HumanScore ++;
-        return "You Win!!! Scissors beats Paper";
+        return ["You Win!!! Scissors beats Paper",  HumanScore, ComputerScore];
     }
     else{
-        return "Draw!!!"
+        return ["Draw!!!", HumanScore, ComputerScore];
     }
 }
 
-console.log(playRound(getHumanChoice(), getComputerChoice()));
-console.log(playRound(getHumanChoice(), getComputerChoice()));
-console.log(playRound(getHumanChoice(), getComputerChoice()));
-console.log(playRound(getHumanChoice(), getComputerChoice()));
-console.log(playRound(getHumanChoice(), getComputerChoice()));
+
+// Main
+function main(){
+    // Scores
+    let HumanScore = 0;
+    let ComputerScore = 0;
+    
+    // Select nodes
+    let HumanScore_span = document.querySelector("#player-score");
+    let ComputerScore_span = document.querySelector("#ai-score");
+    let rock_btn = document.querySelector("#rock");
+    let paper_btn = document.querySelector("#paper");
+    let scissors_btn = document.querySelector("#scissors");
+    let result_div = document.querySelector(".result");
+
+    // Buttons event
+    rock_btn.addEventListener("click", function(){
+        let result = playRound("ROCK", getComputerChoice(), HumanScore, ComputerScore);
+
+        // Create div that stores result text
+        let output_div = document.createElement("div");
+        output_div.textContent = result[0];
+
+        // replace old children
+        result_div.replaceChildren(output_div);
+
+        // Update Score
+        HumanScore = result[1];
+        ComputerScore = result[2];
+
+        
+        HumanScore_span.textContent = HumanScore;
+        ComputerScore_span.textContent = ComputerScore;
+
+        if(HumanScore == 5){
+        // restart if there is a winner
+            HumanScore = 0;
+            ComputerScore = 0;
+            HumanScore_span.textContent = 0;
+            ComputerScore_span.textContent = 0;
+
+            alert("Human is the Winner!!!");
+        }
+
+        if(ComputerScore == 5){
+            // restart if there is a winner
+            HumanScore = 0;
+            ComputerScore = 0;
+            HumanScore_span.textContent = 0;
+            ComputerScore_span.textContent = 0;
+
+            alert("Computer is the Winner!!!");
+        }
+    });
+    
+
+    paper_btn.addEventListener("click", function(){
+        let result = playRound("PAPER", getComputerChoice(), HumanScore, ComputerScore);
+
+        // Create div that stores result text
+        let output_div = document.createElement("div");
+        output_div.textContent = result[0];
+
+        // replace old children
+        result_div.replaceChildren(output_div);
+
+        // Update Score
+        HumanScore = result[1];
+        ComputerScore = result[2];
+
+        
+        HumanScore_span.textContent = HumanScore;
+        ComputerScore_span.textContent = ComputerScore;
+
+        if(HumanScore == 5){
+            // restart if there is a winner
+            HumanScore = 0;
+            ComputerScore = 0;
+            HumanScore_span.textContent = 0;
+            ComputerScore_span.textContent = 0;
+            result_div.replaceChildren();
+
+            alert("Human is the Winner!!!");
+        }
+
+        if(ComputerScore == 5){
+            // restart if there is a winner
+            HumanScore = 0;
+            ComputerScore = 0;
+            HumanScore_span.textContent = 0;
+            ComputerScore_span.textContent = 0;
+            result_div.replaceChildren();
+
+            alert("Computer is the Winner!!!");
+        }
+    });
+
+    
+    scissors_btn.addEventListener("click", function(){
+        let result = playRound("SCISSORS", getComputerChoice(), HumanScore, ComputerScore);
+
+        // Create div that stores result text
+        let output_div = document.createElement("div");
+        output_div.textContent = result[0];
+
+        // replace old children
+        result_div.replaceChildren(output_div);
+
+        // Update Score
+        HumanScore = result[1];
+        ComputerScore = result[2];
+
+        
+        HumanScore_span.textContent = HumanScore;
+        ComputerScore_span.textContent = ComputerScore;
+        
+        if(HumanScore == 5){
+            // restart if there is a winner
+            HumanScore = 0;
+            ComputerScore = 0;
+            HumanScore_span.textContent = 0;
+            ComputerScore_span.textContent = 0;
+            result_div.replaceChildren();
+
+            alert("Human is the Winner!!!");
+        }
+
+        if(ComputerScore == 5){
+            // restart if there is a winner
+            HumanScore = 0;
+            ComputerScore = 0;
+            HumanScore_span.textContent = 0;
+            ComputerScore_span.textContent = 0;
+            result_div.replaceChildren();
+
+            alert("Computer is the Winner!!!");
+        }
+    });
+
+
+}
+
+main()
+
 
 
