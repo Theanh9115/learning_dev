1d3de91e8267a7929a23e01fbef5557cdd0f2d0f
The Anh Nguyen <anhngy9115@gmail.com>
2026-08-17 17:10:51 -0400

    Add increase opacity when visiting a square

--- odin_etch_a_sketch/script.js
+++ odin_etch_a_sketch/script.js
@@ -1,34 +1,60 @@
-let container_div = document.querySelector(".container");
+let containerDiv = document.querySelector(".container");
 
-let grid_size = 16;
+let gridSize = 16;
 
-for (let i = 0; i < grid_size * grid_size; i++){
-    let child_div = document.createElement("div");
-    child_div.classList.add("child")
-    container_div.appendChild(child_div);
-    container_div.setAttribute("style", `grid-template-columns: repeat(${grid_size}, 1fr); grid-template-rows: repeat(${grid_size}, 1fr)`)
+let isHolding = false;
+document.addEventListener("mousedown", () => (isHolding = true));
+document.addEventListener("mouseup", () => (isHolding = false));
+
+let childOpacity = new Map();
+for (let i = 0; i < gridSize * gridSize; i++) {
+  let childDiv = document.createElement("div");
+  childOpacity.set(childDiv, 0);
+  childDiv.addEventListener("mouseenter", function () {
+    if (isHolding) {
+      if (childOpacity.get(childDiv) <= 0.9)
+        childOpacity.set(childDiv, childOpacity.get(childDiv) + 0.1);
+      childDiv.setAttribute("style", `opacity: ${childOpacity.get(childDiv)}`);
+    }
+  });
+  childDiv.classList.add("child");
+  containerDiv.appendChild(childDiv);
+  containerDiv.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr)`,
+  );
 }
 
-let setting_btn = document.querySelector("#setting-button");
-setting_btn.addEventListener("click", function(){
-    grid_size = Number(prompt("How many number of square per side?"));
-    console.log(grid_size);
-    if (grid_size > 100){
-        grid_size = 16;
-    }
-    else {
-        // Clear and reassign
-        container_div.replaceChildren();
-        for (let i = 0; i < grid_size * grid_size; i++){
-            let child_div = document.createElement("div");
-            child_div.classList.add("child")
-            container_div.appendChild(child_div);
+let settingBtn = document.querySelector("#setting-button");
+settingBtn.addEventListener("click", function () {
+  gridSize = Number(prompt("How many number of square per side?"));
+  console.log(gridSize);
+  if (gridSize > 100) {
+    gridSize = 16;
+  } else {
+    // Clear and reassign
+    containerDiv.replaceChildren();
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+      let childDiv = document.createElement("div");
+      childOpacity.set(childDiv, 0);
+      childDiv.addEventListener("mouseenter", function () {
+        if (isHolding) {
+          if (childOpacity.get(childDiv) <= 0.9)
+            childOpacity.set(childDiv, childOpacity.get(childDiv) + 0.1);
+          childDiv.setAttribute(
+            "style",
+            `opacity: ${childOpacity.get(childDiv)}`,
+          );
         }
-        container_div.setAttribute("style", `grid-template-columns: repeat(${grid_size}, 1fr); grid-template-rows: repeat(${grid_size}, 1fr)`)
+      });
+      childDiv.classList.add("child");
+      containerDiv.appendChild(childDiv);
     }
-    console.log(container_div.querySelectorAll("div").length);
+    containerDiv.setAttribute(
+      "style",
+      `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr)`,
+    );
+  }
+  console.log(containerDiv.querySelectorAll("div").length);
 });
-
-
-
-
